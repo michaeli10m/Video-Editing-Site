0c91a65f59477ab6a08cde0a9e904cc4f65f02db
michaeli10m <81631415+michaeli10m@users.noreply.github.com>
2021-05-27 20:10:56 +0300

    Add files via upload
    latest

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,1 @@
 'use strict'
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav-bar") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-bar";
-    }
-}
