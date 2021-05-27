f51bf7df9b1194f1dbe375d7cf7f43bc13bcc952
michaeli10m <81631415+michaeli10m@users.noreply.github.com>
2021-05-27 14:01:24 +0300

    yes

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav-bar") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-bar";
+    }
+}
