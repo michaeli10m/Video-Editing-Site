7900a22f0d885531fdeec7f0be4f695f4cc4a311
michaeli10m <81631415+michaeli10m@users.noreply.github.com>
2021-05-29 17:05:17 +0300

    last

---
+++ JS/contact.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 31.7683, lng: 35.2137 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
