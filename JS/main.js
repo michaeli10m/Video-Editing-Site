6bfb0b6ed43675e432ec4991595139f5a48d317f
michaeli10m <81631415+michaeli10m@users.noreply.github.com>
2021-05-31 18:52:06 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,8 @@
 'use strict'
+window.addEventListener('load', () => {
+    LazyIcon().observe({
+        'src': '/include/scss/fontawesome/font-awesome.min.css',
+        'selector': '.fa',
+        'rootMargin': '150px 0px'
+    });
+});
